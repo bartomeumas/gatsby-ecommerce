d081e39ab4367e08c93bf386e506f97ec6f7449c
bartomeumas <bartomeumas00@gmail.com>
2022-02-11 13:50:21 -0400

    fixed sidebar import

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,7 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import "bootstrap/dist/css/bootstrap.min.css"
 
-import SideBar from "./sidebar"
+import SideBar from "./SideBar"
 import "./layout.css"
 
 const Layout = ({ children }) => {
